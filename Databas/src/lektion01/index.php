d0562d1ea56e3635d08e94aec268fe99db40f190
Elvira Pilblad <elvirapihlblad@hotmail.com>
2024-11-26 14:11:14 +0100

    lektion01

---
+++ Databas/src/lektion01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Hello Webb2024
+    </h1>
+
+    <? echo 'Hello World';?>
+
+
+<?php
+// Beräkna produkten
+$resultat = 234 * 923;
+
+// Skriv ut resultatet
+echo "Resultatet av 234 gånger 923 är: " . $resultat;
+?>
+
+</body>
+</html>
